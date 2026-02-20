6864c985703db03a7c72a27df5c8cdf06fc2ed15
wachiraya301045 <wachiraya.tunp@bumail.net>
2026-02-20 16:43:22 +0700

    Exam02

--- Assets/Scripts/Exam/PlayerControllerExam02.cs
+++ Assets/Scripts/Exam/PlayerControllerExam02.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerControllerExam02 : MonoBehaviour
 {
     public float speed;
-    public float zRange = 10;
+    public float yRange = 10;
     public GameObject projectilePrefab;
 
     private float verticalInput;
@@ -21,5 +21,21 @@ public class PlayerControllerExam02 : MonoBehaviour
     void Update()
     {
         verticalInput = moveAction.ReadValue<Vector2>().y;
+        
+        transform.Translate(verticalInput * speed * Time.deltaTime * Vector3.right);
+
+        if (transform.position.x < -yRange)
+        {
+            transform.position = new Vector3(-yRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > yRange)
+        {
+            transform.position = new Vector3(yRange, transform.position.y, transform.position.z);
+        }
+        if (shootAction.triggered)
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
